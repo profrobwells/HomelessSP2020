60404251079319cf3755dfd50ff8759483515823
Rob Wells <robwells@ARSC-7061275.local>
2020-02-06 13:50:59 -0600

    Update Exercise for Feb 10 class

--- Exercises/SF 311 Calls.R
+++ Exercises/SF 311 Calls.R
@@ -47,6 +47,93 @@ Types <- SF %>% count(original_crime_type_name) %>%
 #Question #4: What are the top five complaints in this data and provide the number of complaints. 
 #Create a filter statement to produce a table with just the top five results from the Types table.
 #Put your code and paste the table below.
+#
+
+#-----------------------------------------
+#Part 2: San Francisco 311 Call for Service Data
+#Feb 5, 2020
+#Filtering 
+#Building Tables
+#Basic charts
+
+
+
+#Exercise: Number of complaints per year
+#Recall from line 48 the table that counted by days, or call_date2
+# Days <- SF %>% 
+#   count(call_date2) %>% 
+#   group_by(call_date2) %>% 
+#   arrange(desc(n))
+
+#How to export a table into a spreadsheet (csv is a comma separated file)
+write.csv(Days,"Days.csv")
+
+#QUESTION #5: Now build a similar table to the Days table totalling the number of complaints by year
+#WRITE YOUR ANSWER HERE AND SUPPLY THE CODE
+#Export the finished version to a spreadsheet
+
+
+#------------------------------
+#EXERCISE: Grouping by disposition
+#Look at the Radio Codes spreadsheet under dispositions
+#https://data.sfgov.org/api/views/hz9m-tj6z/files/b60ee24c-ae7e-4f0b-a8d5-8f4bd29bf1de?download=true&filename=Radio%20Codes%202016.xlsx
+#Total by disposition
+
+Action <- SF %>% 
+  count(disposition) %>% 
+  arrange(desc(n))
+
+#QUESTION #6: Describe in one paragraph some of the common trends illustrated in the disposition data and any questions you may have for the police about this data
+#WRITE YOUR ANSWER HERE 
+
+#------------------------------
+#EXERCISE: Create a table with serious infractions described in disposition
+
+#Example: Here's a table filtering the dispositions column to show "no disposition" or "gone on arrival"
+Nopeople <- SF %>% 
+  filter(disposition == "ND" | disposition == "GOA")
+
+#QUESTION #7: Create a table with the serious actions including citations and arrests police took in the dispositions
+
+
+#------------------------------
+#EXERCISE - A Basic chart of the crime data
+ggplot(Years, aes(x = year, y = n)) + 
+  geom_bar(stat = "identity") +
+  #coord_flip() +    #this makes it a horizontal bar chart instead of vertical
+  labs(title = "Homeless Calls Per Year, San Francisco", 
+       subtitle = "311 Call Data, 3/2016-11/2019",
+       caption = "Graphic by Wells",
+       y="Number of Calls",
+       x="Year")
+
+#A chart using a dplyr filtering language
+Years %>% 
+  filter(year >= 2017) %>% 
+  ggplot(aes(x = year, y = n)) +
+  geom_bar(stat = "identity") +
+  #coord_flip() +    #this makes it a horizontal bar chart instead of vertical
+  labs(title = "Homeless Calls After 2017, San Francisco", 
+       subtitle = "311 Call Data, 2017-2019",
+       caption = "Graphic by Wells",
+       y="Number of Calls",
+       x="Year")
+
+#A more complex filter
+SF %>% 
+  filter(!is.na(common_location)) %>% 
+  count(common_location) %>% 
+  top_n(10, n) %>% 
+  ggplot(aes(x = common_location, y = n)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +    #this makes it a horizontal bar chart instead of vertical
+  labs(title = "Popular Spots for Homeless, San Francisco", 
+       subtitle = "311 Call Data, 2016-2019",
+       caption = "Graphic by Wells",
+       y="Number of Calls",
+       x="Places")
+
+#QUESTION #8: Chart the total dispositions. Filter for at least 100 actions. Add color, export image to Blackboard.
 
 
 
